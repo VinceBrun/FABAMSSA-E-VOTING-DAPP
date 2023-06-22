d5d2d3af70741d2b1b2df63e4883ced09c2e3b1c
VinceBruno <ambavincent1203@gmail.com>
2023-06-22 11:35:58 +0100

    client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,26 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
           Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
         </h1>
 
-        <p className="description">
+        <p className={styles.description}>
           Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
+          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
+          <code className={styles.code}>pages/index.js</code> file!
         </p>
 
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
+        <div className={styles.connect}>
+          <ConnectWallet />
         </div>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
+        <div className={styles.grid}>
+          <a href="https://portal.thirdweb.com/" className={styles.card}>
             <h2>Portal &rarr;</h2>
             <p>
               Guides, references and resources that will help you build with
@@ -28,7 +28,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://thirdweb.com/dashboard" className="card">
+          <a href="https://thirdweb.com/dashboard" className={styles.card}>
             <h2>Dashboard &rarr;</h2>
             <p>
               Deploy, configure and manage your smart contracts from the
@@ -36,7 +36,10 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://portal.thirdweb.com/templates" className="card">
+          <a
+            href="https://portal.thirdweb.com/templates"
+            className={styles.card}
+          >
             <h2>Templates &rarr;</h2>
             <p>
               Discover and clone template projects showcasing thirdweb features.
